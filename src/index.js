ddb3f9a5fbeba1fdceeebab8af3d1126c85fe965
yoonthe <yuzaiyang@flashhold.com>
2020-01-27 03:57:44 +0800

    添加 视频过渡效果

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef } from 'react';
 import { Application, Texture, Sprite, filters } from 'pixi.js';
 import video from './yangge_star.mp4';
+import video1 from './zhaohe.mp4';
 import styles from './index.less';
 // import Loading from './components/Loading';
 import Header from './components/Header';
@@ -8,6 +9,30 @@ import Start from './components/Start';
 import audio from './love_heart.mp3';
 import Circle from './components/Circle';
 import { throttle } from './utils';
+
+const FPS = 30;
+
+const setDarkAnimation = (ticker, gotoDark, callback) => {
+  const darkFilter = new filters.ColorMatrixFilter();
+  let t = FPS;
+  const setBrightness = gotoDark ? step => darkFilter.brightness(step) : step => darkFilter.brightness(1 - step);
+  const animate = delta => {
+    if (t < 0) {
+      return;
+    }
+    t -= delta;
+    if (t <= 0) {
+      if (typeof callback === 'function') {
+        callback();
+      }
+      setBrightness(0);
+      ticker.remove(animate);
+    }
+    setBrightness(t / FPS);
+  };
+  ticker.add(animate);
+  return darkFilter;
+};
 
 const content = [
   '#2019#',
@@ -18,7 +43,7 @@ const content = [
 const cards = [];
 for (let i = 0; i < 6; i++) {
   cards.push({
-    background: video,
+    background: i % 2 === 1 ? video1 : video,
     title: `#伴我同行#STAND BY ME#${i}#`,
     content,
   });
@@ -39,31 +64,40 @@ export default function GreetingCards() {
     document.body.parentNode.style = `font-size: ${18 + 4 * window.devicePixelRatio}px`;
     const { innerWidth: width, innerHeight: height } = window;
     const app = new Application({ width, height });
-    const { stage } = app;
+    const { stage, ticker } = app;
     pixiContainer.current.appendChild(app.view);
     setFns({
       upload: src => {
         if (src === null) {
           return;
         }
-        stage.removeChildren();
-        const t = Texture.from(src);
-        const s = new Sprite(t);
-        s.name = 'background';
-        const filter = new filters.BlurFilter();
-        filter.blur = 8;
-        s.filters = [filter];
-        const { resource } = t.baseTexture;
-        if (resource && resource.source) {
-          const { source } = resource;
-          source.muted = true;
-          source.loop = true;
-          source.autoplay = true;
-          setVideoSource(source);
+        const nextBackground = () => {
+          stage.removeChildren();
+          const texture = Texture.from(src);
+          const s = new Sprite(texture);
+          s.name = 'background';
+          const blurFilter = new filters.BlurFilter();
+          
+          blurFilter.blur = 8;
+          s.filters = [blurFilter, setDarkAnimation(ticker, false)];
+          const { resource } = texture.baseTexture;
+          if (resource && resource.source) {
+            const { source } = resource;
+            source.muted = true;
+            source.loop = true;
+            source.autoplay = true;
+            setVideoSource(source);
+          }
+          s.width = width;
+          s.height = height;
+          stage.addChild(s);
+        };
+        const last = stage.getChildByName('background');
+        if (last) {
+          last.filters[1] = setDarkAnimation(ticker, true, nextBackground);
+        } else {
+          nextBackground();
         }
-        s.width = width;
-        s.height = height;
-        stage.addChild(s);
       },
     });
   }, []);
